2b2143ea7c759efcb7537bbfb92e1460106c2beb
kulcsarbence <kulcsar.bence@different.hu>
2022-12-21 14:46:33 +0100

    demo project update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Different\DifferentCore\app\Http\Middlewares\SetLangMiddleware::class,
         ],
 
         'api' => [
